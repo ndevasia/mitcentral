9a2e0a9562dc84c5eb5f4c8590d68993bb812976
jonsh <jonsh@mit.edu>
2020-09-19 19:11:41 -0400

    Clean up code

--- js/load-data.js
+++ js/load-data.js
@@ -6,24 +6,4 @@ function loadClasses() {
           console.log("out");
         }
       });
-      
-    /*console.log("loading");
-    const user = firebase.auth().currentUser;
-    if (user === null) {
-        // user is not logged in, no classes
-        console.log("not logged in");
-        return;
-    }
-    console.log(user);
-    const userInfo = db.collection("users").doc(user);
-    userInfo.get().then(function(doc) {
-        if (doc.exists) {
-            console.log("Document data:", doc.data());
-        } else {
-            // doc.data() will be undefined in this case
-            console.log("No such document!");
-        }
-    }).catch(function(error) {
-        console.log("Error getting document:", error);
-    });*/
 }
