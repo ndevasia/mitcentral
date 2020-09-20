e7b09ed08892701f19e4880f2fea3ff8c0eaaa05
jonsh <jonsh@mit.edu>
2020-09-19 23:42:27 -0400

    Rename add-class to store-data and implement add-platform

---
+++ js/store-data.js
@@ -0,0 +1,72 @@
+possibleLinks = [['#lecture-link-textbox', 'Lecture'], 
+		['#recitation-link-textbox', 'Recitation'], 
+		['#lab-link-textbox', 'Lab'],
+		['#class-website-textbox', 'Website'],
+		['#OH-link-textbox', 'Office Hours']]
+
+function addClass() {
+	firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+
+        	const db = firebase.firestore();
+			userEmail = db.collection("users").doc(user.email);
+			className = document.querySelector('#class-name-textbox').value;
+			classColor = document.querySelector('#class-color').value;
+			actualLinks = []
+			possibleLinks.forEach((possibleLink) => {
+				const source = document.querySelector(possibleLink[0]);
+				if (source !== null) {
+					const linkInfo = source.value;
+					if (linkInfo !== '') {
+						actualLinks.push({'name': possibleLink[1],
+							'link': source.value});
+					}
+				}
+			});
+			
+        	userEmail.update({
+			    classes: firebase.firestore.FieldValue.arrayUnion(
+				    {
+				    	name: className,
+				    	color: classColor,
+				    	links: actualLinks,
+				    }
+			    )
+			});
+                    
+        	return false;
+        }
+        else {
+        	console.log("logged out");
+        }
+	});
+}
+
+function addPlatform() { 
+	firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+
+        	const db = firebase.firestore();
+			userEmail = db.collection("users").doc(user.email);
+
+			platformName = document.querySelector('#plat-name-submit').value;
+			platformColor = document.querySelector('#plat-color-submit').value;
+			platformLink = document.querySelector('#plat-link-submit').value;
+			
+        	userEmail.update({
+			    platforms: firebase.firestore.FieldValue.arrayUnion(
+				    {
+				    	name: platformName,
+				    	color: platformColor,
+				    	link: platformLink,
+				    }
+			    )
+			});
+                    
+        	return false;
+        }
+        else {
+        	console.log("logged out");
+        }
+	});
+}
