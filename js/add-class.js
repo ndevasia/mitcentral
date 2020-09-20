c885fdd2d5323cc83a78f4c3a2299a55f023f998
shulijones <59156391+shulijones@users.noreply.github.com>
2020-09-19 23:34:08 -0400

    Final bug fixes to class adding function

--- js/add-class.js
+++ js/add-class.js
@@ -16,12 +16,9 @@ function addClass() {
 			actualLinks = []
 			possibleLinks.forEach((possibleLink) => {
 				const source = document.querySelector(possibleLink[0]);
-				console.log('source :' + source);
 				if (source !== null) {
 					const linkInfo = source.value;
-					console.log(linkInfo);
 					if (linkInfo !== '') {
-						console.log(possibleLink[1]);
 						actualLinks.push({'name': possibleLink[1],
 							'link': source.value});
 					}
