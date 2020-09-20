46cc0e40e030f81fee5b6e582c041f3ac51cedf5
EmilyCaragay <caragayei@gmail.com>
2020-09-19 22:45:47 -0400

    fixed merge bug, actually working add w/o redirect

--- js/add-class.js
+++ js/add-class.js
@@ -7,24 +7,24 @@ function addClass(){
 			userEmail = db.collection("users").doc(user.email);
 			
 			className = document.querySelector('#class-name-textbox').value;
-			classColor = document.querySelector('#class-color').value;
-			lectureLink = document.querySelector('#lecture-link-textbox').value;
-			recitationLink = document.querySelector('#recitation-link-textbox').value;
-			labLink = document.querySelector('#lab-link-textbox').value;
-			classWebsiteLink = document.querySelector('#class-website-textbox').value;
-			officeHoursLink = document.querySelector('#OH-link-textbox').value;
+			// classColor = document.querySelector('#class-color').value;
+			// lectureLink = document.querySelector('#lecture-link-textbox').value;
+			// recitationLink = document.querySelector('#recitation-link-textbox').value;
+			// labLink = document.querySelector('#lab-link-textbox').value;
+			// classWebsiteLink = document.querySelector('#class-website-textbox').value;
+			// officeHoursLink = document.querySelector('#OH-link-textbox').value;
 			
         	userEmail.update({
 			    classes: firebase.firestore.FieldValue.arrayUnion(
 				    {
 				    	name: className,
-				    	color: classColor,
+				    	color: "blue",
 				    	links: [
-								{'name': 'Lecture', 'link': lectureLink},
-								{'name': 'Recitation', 'link': recitationLink},
-								{'name': 'Lab', 'link': labLink},
-								{'name': 'Website', 'link': classWebsiteLink},
-								{'name': 'Office Hours', 'link': officeHoursLink}
+								// {'name': 'Lecture', 'link': lectureLink},
+								// {'name': 'Recitation', 'link': recitationLink},
+								// {'name': 'Lab', 'link': labLink},
+								// {'name': 'Website', 'link': classWebsiteLink},
+								// {'name': 'Office Hours', 'link': officeHoursLink}
 				    		]
 				    }
 			    )
